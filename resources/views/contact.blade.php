0354f01596ba479689c6e5bd45a859c68d8e3b17
KiselevP <kiselev.random@yandex.ru>
2024-01-31 17:39:22 +0300

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.app')
+
+@section('title-block')
+    Страница контактов
+@endsection
+
+@section('content')
+    <h1>Страница контактов</h1>
+
+    <form action="{{ route('contact-form') }}" method="post">
+    @csrf
+
+        <div class="form-group">
+            <label for="name">Ведите имя</label>
+            <input class="form-control" type="text" name="name" placeholder="Введите имя" id="name">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Ведите email</label>
+            <input class="form-control" type="text" name="email" placeholder="Введите email" id="email">
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input class="form-control" type="text" name="subject" placeholder="Тема сообщения" id="subject">
+        </div>
+
+        <div class="form-group">
+            <label for="massage">Сообщение</label>
+            <textarea class="form-control" name="massage" placeholder="Введите сообщение" id="massage"></textarea>
+        </div>
+
+        <button class="btn btn-success" type="submit">Отправить</button>
+    </form>
+@endsection
---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,8 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 m-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight0normal">Test Project</h5>
+    <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="p-2 text-dark" href="{{route('home')}}">Главная</a>
+        <a class="p-2 text-dark" href="{{route('about')}}">Про нас</a>
+        <a class="p-2 text-dark" href="{{route('contact')}}">Контакты</a>
+    </nav>
+</div>
